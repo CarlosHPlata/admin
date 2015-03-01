466b9e872cac79454f18d2fce4eb9909a8875518
jose_ramon <slayermx_117@hotmail.com>
2015-02-28 21:30:34 -0600

    Label ya no es un entitie

--- app/src/main/java/com/model/Label.java
+++ app/src/main/java/com/model/Label.java
@@ -6,33 +6,10 @@ import android.database.Cursor;
 /**
  * Created by José Ramón Díaz on 28/02/2015.
  */
-public class Label extends Entitie {
+public class Label{
 
     public Label(String title){
-        super(tableName, columNames);
        this.title = title;
-    }
-
-    public Label(){
-        super(tableName, columNames);
-    }
-
-    @Override
-    public ContentValues getContentValues() {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(columNames[TITLE_POSITION], title);
-        return contentValues;
-    }
-
-    @Override
-    public void setContentValues(Cursor cursor) {
-        setId(cursor.getInt(ID_POSITION));
-        title = cursor.getString(TITLE_POSITION);
-    }
-
-    @Override
-    public Entitie getNewInstance() {
-        return new Label();
     }
 
     public String getTitle() {
@@ -43,9 +20,14 @@ public class Label extends Entitie {
         this.title = title;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
     private String title;
-    private static final String tableName = "labels";
-    private static final String[] columNames = {"id","title"};
-    private static final int ID_POSITION = 0;
-    private static final int TITLE_POSITION = 1;
 }
